c8ba615fb50203370330c37544f97226ba350d3f
Minal <patilminalp@gmail.com>
2021-06-19 08:00:01 +0530

    This is Utility problem

---
+++ Utility.java
@@ -0,0 +1,11 @@
+package com.program;
+import java.util.Scanner;
+public class Utility {
+    private static final Scanner sc = new Scanner(System.in);
+
+
+    public static int getUserSpecificInteger() {
+        return sc.nextInt();
+    }
+
+}
